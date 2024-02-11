f3ee4f8260c6ee92470842f00358fef280bcbbaa
Sami Khan <sami999khan999@gmail.com>
2024-02-12 01:24:51 +0600

    user auth done

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,19 +2,21 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-// let cached = (global as any).mongoose || { conn: null, promise: null };
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-  // if (cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
   if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
-  const cached = mongoose.connect(MONGODB_URI, {
-    dbName: "event_horizon",
-    bufferCommands: false,
-  });
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "event_horizon",
+      bufferCommands: false,
+    });
 
-  const promise = await cached;
+  cached.conn = await cached.promise;
 
-  return promise;
+  return cached.conn;
 };
