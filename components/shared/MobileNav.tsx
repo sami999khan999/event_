4397f9b965995b126c18534e4ae2b37208f8260e
Sami Khan <sami999khan999@gmail.com>
2024-02-11 22:26:53 +0600

    done

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -5,38 +5,38 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "@/components/ui/sheet";
-import Image from "next/image";
-import { Separator } from "../ui/separator";
-import { NavItems } from "./NavItems";
+} from "@/components/ui/sheet"
+import Image from "next/image"
+import { Separator } from "../ui/separator"
+import NavItems from "./NavItems"
+
 
 const MobileNav = () => {
   return (
-    <nav className="md:hidden ">
+    <nav className="md:hidden">
       <Sheet>
-        <SheetTrigger className="aling-middle">
-          <Image
+        <SheetTrigger className="align-middle">
+          <Image 
             src="/assets/icons/menu.svg"
-            alt="meny"
+            alt="menu"
             width={24}
             height={24}
             className="cursor-pointer"
           />
         </SheetTrigger>
         <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
-          <Image
+          <Image 
             src="/assets/images/logo.svg"
-            alt="logo "
+            alt="logo"
             width={128}
             height={38}
           />
           <Separator className="border border-gray-50" />
-
           <NavItems />
         </SheetContent>
       </Sheet>
     </nav>
-  );
-};
+  )
+}
 
-export default MobileNav;
+export default MobileNav
